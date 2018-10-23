1a807377abaaea0a9815b7992b989a6a2ad0547c
Maciek <maciek.d@me.com>
2018-10-23 08:22:41 -0700

    Added enum for months

---
+++ ExpensesManager/Models/Months.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesManager.Models
+{
+    public enum Months
+    {
+        January, Feburary, March, April, May, June, July, August, September, October, November, December
+    }
+}
